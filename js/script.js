14fcbaf34d285ec21adc8dfc9ade9b4c25fdab73
Pablo Daniel Sueiro <pablodsueiro@yahoo.com.ar>
2023-01-27 04:22:18 -0300

    v0.9 - Added Dropdown Form

--- js/script.js
+++ js/script.js
@@ -27,3 +27,30 @@ function ReplaceTextSpanish() {
         document.getElementById(translations[i].id).innerHTML = translations[i].es;
     }
 }
+
+function CheckForms(){
+var allInputForms = document.getElementsByClassName("form-control");
+var allInvalidMessages = document.getElementsByClassName("invalid-feedback");
+var allValidMessages = document.getElementsByClassName("valid-feedback");
+var validCount = 0;
+for (i = 0; i<allInputForms.length; i++)
+    {
+      if(allInputForms[i].value === "" )
+      {
+          allInvalidMessages[i].style.display = "contents";
+          allValidMessages[i].style.display = "none";
+
+      }
+      else
+      {
+          allInvalidMessages[i].style.display = "none";
+          allValidMessages[i].style.display = "contents";
+            validCount++;
+            if(validCount===allInputForms.length)
+            document.getElementById("sentMessage").style.display = "contents";
+            else 
+            document.getElementById("sentMessage").style.display = "none";
+      }
+
+    }
+}
